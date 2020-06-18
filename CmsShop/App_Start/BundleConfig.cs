7cb4fa6122530951c6d10524e205378f107429d2
MarcinKwiatkowski04 <46976228+MarcinKwiatkowski04@users.noreply.github.com>
2020-06-18 14:39:28 +0200

    css file included to bundle - previous error repaired

--- CmsShop/App_Start/BundleConfig.cs
+++ CmsShop/App_Start/BundleConfig.cs
@@ -26,7 +26,7 @@ namespace CmsShop
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
 
         }
     }
